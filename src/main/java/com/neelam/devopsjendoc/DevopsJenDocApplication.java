0c4ca3c05c97945a4017fef9f0e9a4bc6d901e71
karthick678 <karthickkumar678@gmail.com>
2024-02-13 19:12:35 +0530

    first commit

---
+++ src/main/java/com/neelam/devopsjendoc/DevopsJenDocApplication.java
@@ -0,0 +1,13 @@
+package com.neelam.devopsjendoc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DevopsJenDocApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DevopsJenDocApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/neelam/devopsjendoc/DevopsJenDocApplicationTests.java
@@ -0,0 +1,13 @@
+package com.neelam.devopsjendoc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DevopsJenDocApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
